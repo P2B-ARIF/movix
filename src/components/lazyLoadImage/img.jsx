83db2a640d48003b740a13dc697d7edc6d106ee2
MD Arif <p2b.business.info@gmail.com>
2023-09-23 00:05:34 +0600

    added

--- src/components/lazyLoadImage/img.jsx
+++ src/components/lazyLoadImage/img.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
+// p2b
+
 const Img = ({ src, className }) => {
 	return (
 		<LazyLoadImage className={className || ""} alt='' effect='blur' src={src} />
